dc59bd9373b02353fb9934e0e88a5c378725a063
Penswick <matthewpengo@gmail.com>
2023-04-20 21:16:19 +0100

    Added shallow water.

--- src/Main.js
+++ src/Main.js
@@ -8,18 +8,16 @@ function generateNoise() {
   let landPixels;
   let totalPixels;
   let landPercentage;
-  const seed1 = Math.floor(Math.random() * 100000); // generates the first seed
-  const seed2 = Math.floor(Math.random() * 100000); // generates the second seed
-  const layerData = generateLayerOne(canvas.width, canvas.height, seed1, seed2);
-  const minLandPercentage = 50; // Minimum percentage of land
+  const minLandPercentage = 30; // Minimum percentage of land
   const maxLandPercentage = 60; // Maximum percentage of land
-  const maxAttempts = 10; // Maximum number of attempts to generate a map
+  const maxAttempts = 100; // Maximum number of attempts to generate a map
   let attempts = 0;
+  const shallowWaterThreshold = 0.05; // change the deep/shallow water ratio
   let imageData;
   
-
   do {
-
+    const seed1 = Math.floor(Math.random() * 100000); // generates the first seed
+    const seed2 = Math.floor(Math.random() * 100000); // generates the second seed
     imageData = ctx.createImageData(canvas.width, canvas.height);
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
@@ -29,6 +27,8 @@ function generateNoise() {
 
     landPixels = 0;
     totalPixels = 0;
+
+    const layerData = generateLayerOne(canvas.width, canvas.height, seed1, seed2);
 
     for (let y = 0; y < canvas.height; y++) {
       for (let x = 0; x < canvas.width; x++) {
@@ -44,13 +44,18 @@ function generateNoise() {
         const blendedNoiseValue = combinedNoiseValue * gradient;
 
         const idx = (y * canvas.width + x) * 4;
-        const colorThreshold = 0.02; // change the water/land ratio
+        const colorThreshold = 0.015; // change the water/land ratio
 
         if (blendedNoiseValue < colorThreshold) {
-          // Water
+          // Deep water
           imageData.data[idx] = 0;
           imageData.data[idx + 1] = 0;
           imageData.data[idx + 2] = 150;
+        } else if (blendedNoiseValue < shallowWaterThreshold) {
+          // Shallow water
+          imageData.data[idx] = 0;
+          imageData.data[idx + 1] = 100;
+          imageData.data[idx + 2] = 255;
         } else {
           // Land
           imageData.data[idx] = 0;
@@ -58,17 +63,22 @@ function generateNoise() {
           imageData.data[idx + 2] = 0;
           landPixels++;
         }
+
+        // Set the alpha channel value to 255
         imageData.data[idx + 3] = 255;
         totalPixels++;
       }
     }
 
     landPercentage = (landPixels / totalPixels) * 100;
+    console.log(`Attempt ${attempts}: ${landPercentage}% land`);
     attempts++; // increments the attempts at generating the map.
   } while ((landPercentage < minLandPercentage || landPercentage > maxLandPercentage) && attempts < maxAttempts);
 
   ctx.putImageData(imageData, 0, 0);
 }
+
+
 
 generateBtn.addEventListener('click', generateNoise);
 
