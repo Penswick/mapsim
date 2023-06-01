786e68e01ba6bd6f9afa7532a44f77056178d8a7
Penswick <matthewpengo@gmail.com>
2023-06-01 16:44:17 +0100

    Initial anchor points implemented.

--- src/layers/LayerThree.js
+++ src/layers/LayerThree.js
@@ -6,50 +6,49 @@ export function generateLayerThree(width, height, seed4, heatMapContainer) {
   let desertCount = 0;
   let defaultCount = 0;
 
-// Defines the size of the grid cells. Don't go below 2. Default is 25.
-const cellSize = 25;
+  const cellSize = 25;
 
-// Creates a 2D array to store the random numbers for each cell
-const cellValues = Array.from(Array(Math.ceil(height/cellSize)), () => new Array(Math.ceil(width/cellSize)));
+  const cellValues = Array.from(Array(Math.ceil(height/cellSize)), () => new Array(Math.ceil(width/cellSize)));
 
-// Generates a random number between -5 and 5 for each cell
-for (let y = 0; y < Math.ceil(height/cellSize); y++) {
-  for (let x = 0; x < Math.ceil(width/cellSize); x++) {
-    cellValues[y][x] = Math.floor(Math.random() * 11) - 5;
+  for (let y = 0; y < Math.ceil(height/cellSize); y++) {
+    for (let x = 0; x < Math.ceil(width/cellSize); x++) {
+      // cellValues[y][x] = Math.floor(Math.random() * 11) - 5;
+      cellValues[y][x] = 0; // Change here, all generated values are now 0
+    }
   }
-}
 
-  // Creates a canvas for the heatmap grid
   const canvas = document.createElement('canvas');
   canvas.width = width;
   canvas.height = height;
   const ctx = canvas.getContext('2d');
 
-  // Draws the grid on the canvas
-  ctx.strokeStyle = 'black'; // Sets grid color
-  ctx.lineWidth = 0.5; // Sets grid line width
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 0.5;
 
-  ctx.font = 'bold 14px Arial'; // Sets font for numbers
-  ctx.fillStyle = 'white'; // Sets color for numbers
+  ctx.font = 'bold 14px Arial';
+  ctx.fillStyle = 'white';
 
-// Draws the grid on the canvas and add the cell values
-for (let y = 0; y < Math.ceil(height/cellSize); y++) {
-  for (let x = 0; x < Math.ceil(width/cellSize); x++) {
-    // Draws cell boundaries
-    ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
-    
-    const cellValue = cellValues[y][x];
+  for (let y = 0; y < Math.ceil(height/cellSize); y++) {
+    for (let x = 0; x < Math.ceil(width/cellSize); x++) {
+      ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
+      
+      const cellValue = cellValues[y][x];
 
-    // Calculates the center position of each cell for the text
-    const textX = x * cellSize + cellSize / 2;
-    const textY = y * cellSize + cellSize / 2;
+      const textX = x * cellSize + cellSize / 2;
+      const textY = y * cellSize + cellSize / 2;
 
-    // Adds the number to the center of the cell
-    ctx.fillText(cellValue, textX, textY);
+      ctx.fillText(cellValue, textX, textY);
+    }
   }
-}
+  // draws the anchor points
+  ctx.font = 'bold 25px Arial';
+  ctx.fillStyle = 'red'
 
-
+  // Draw the direction letters
+  ctx.fillText('N', width / 2, 50); // N at the top
+  ctx.fillText('S', width / 2, height - 50); // S at the bottom
+  ctx.fillText('W', 50, height / 2); // W at the left
+  ctx.fillText('E', width - 50, height / 2); // E at the right
 
 for (let y = 0; y < Math.ceil(height/cellSize); y++) {
   for (let x = 0; x < Math.ceil(width/cellSize); x++) {
@@ -84,7 +83,6 @@ for (let y = 0; y < Math.ceil(height/cellSize); y++) {
     }
   }
 }
-
 
   heatMapContainer.innerHTML = ''; // Clear the heatMapContainer
   heatMapContainer.appendChild(canvas); // Append the new canvas to the heatMapContainer
